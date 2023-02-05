d8218bb18c356a2cf44ab721ba0210bc8008f623
Iceblok <ycoskun0318@gmail.com>
2023-02-05 20:57:52 +0100

    Initial commit

---
+++ 1.1_HW.py
@@ -0,0 +1,29 @@
+"""
+==========================[ Huiswerk 1.1 ]================================
+"""
+
+print('\n')
+
+print('Opgave 2.1')
+print('a) propositie')
+print('b) propositie')
+print('c) propositie')
+print('d) propositie')
+print('e) geen propositie')
+print('f) propositie')
+print('g) geen propositie')
+print('h) propositie')
+print('i) propositie')
+
+print('\n')
+
+print('Opgave 2.2')
+print('a) p = Jan droomt, q = Jan slaapt\np → q')
+print('b) p = slapen, q = dromen\np → q')
+print('c) p = drinken, q = dronken\n¬p → ¬q')
+print('d) p = gedronken, r = rijden, q = forse boete\n(p ∧ r) → q')
+print('e) p = gedronken, r = rijden, q = forse boete, '
+      'a = het promillage alcohol in mijn bloed onder een bepaalde waarde ligt\n(p ∧ r) → (q ∨ a)')
+print('f) r = rijden, p = gedronken, x = onder invloed van XTC, q = onveilig op de weg\nr ∧ (p ∨ x) → q')
+
+print('Opgave 2.3')
